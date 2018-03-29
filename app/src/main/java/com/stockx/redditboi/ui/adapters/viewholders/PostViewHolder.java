28f7ba3f2b47bb13385ac180566ba9245e83293e
oliver <oliver.goldbart@gmail.com>
2018-03-29 14:05:24 -0400

    added images

--- app/src/main/java/com/stockx/redditboi/ui/adapters/viewholders/PostViewHolder.java
+++ app/src/main/java/com/stockx/redditboi/ui/adapters/viewholders/PostViewHolder.java
@@ -1,8 +1,10 @@
 package com.stockx.redditboi.ui.adapters.viewholders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.stockx.redditboi.R;
 import com.stockx.redditboi.listeners.GenericListener;
 import com.stockx.redditboi.model.RedditPost;
@@ -13,6 +15,7 @@ import com.stockx.redditboi.model.RedditPost;
 
 public class PostViewHolder extends BaseViewHolder<RedditPost> {
 
+    private ImageView mPreviewImageView;
     private TextView mTitleTextView;
 
     private RedditPost mRedditPost;
@@ -20,12 +23,33 @@ public class PostViewHolder extends BaseViewHolder<RedditPost> {
     public PostViewHolder(View itemView, GenericListener<RedditPost> itemClickListener) {
         super(itemView, itemClickListener);
 
+        mPreviewImageView = itemView.findViewById(R.id.post_preview);
         mTitleTextView = itemView.findViewById(R.id.post_title);
     }
 
     @Override
     public void bind(RedditPost redditPost) {
         mRedditPost = redditPost;
+
+        if (mRedditPost != null
+                && mRedditPost.getData() != null
+                && mRedditPost.getData().getPreview() != null
+                && mRedditPost.getData().getPreview().getRedditImages() != null
+                && mRedditPost.getData().getPreview().getRedditImages().size() > 0
+                && mRedditPost.getData().getPreview().getRedditImages().get(0) != null
+                && mRedditPost.getData().getPreview().getRedditImages().get(0).getSource() != null
+                && mRedditPost.getData().getPreview().getRedditImages().get(0).getSource().getUrl() != null
+                && !mRedditPost.getData().getPreview().getRedditImages().get(0).getSource().getUrl().isEmpty()) {
+            mPreviewImageView.setVisibility(View.VISIBLE);
+            Picasso.with(mContext)
+                    .load(mRedditPost.getData().getPreview().getRedditImages().get(0).getSource().getUrl())
+                    .placeholder(R.drawable.ic_error_black_24dp)
+                    .fit()
+                    .centerCrop()
+                    .into(mPreviewImageView);
+        } else {
+            mPreviewImageView.setVisibility(View.GONE);
+        }
 
         mTitleTextView.setText(redditPost.getData().getTitle());
 
